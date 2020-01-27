3e29b60a394401b48a972d159417104b98f00abc
Kyle Rassweiler <kylerassweiler@gmail.com>
2020-01-27 10:08:03 -0500

    Removed pandas, setup intra day calls

--- main.py
+++ main.py
@@ -3,33 +3,10 @@ import os
 import time
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
-if sys.platform[:3] == 'win':
-    from msvcrt import getch
-import pandas as pd
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.techindicators import TechIndicators
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plot
-
 from wallet.wallet import Wallet
 from stock.stock import Stock
-
-period = 60
-#data, meta = ts.get_intraday(
-#    symbol='APHA.TO', interval='1min', outputsize='full')
-#ti = TechIndicators(key=os.environ['AlphaKey'], output_format='pandas')
-#dataTi, metaTi = ti.get_sma(
-#    symbol='APHA.TO', interval='1min', time_period=period, series_type='close')
-#df1 = dataTi
-#df2 = data['4. close'].iloc[period-1::]
-#df = pd.concat([df1, df2], axis=1)
-#df.plot()
-#plot.show()
-
-key = 0
-#while key != 27:
-    #key = ord(getch())
 
 def ParseSite(numberOfStocks=None,maximumStockPrice=0, minimumStockPrice=0):
     site = 'https://ca.finance.yahoo.com/most-active?offset=0&count=100'
@@ -62,7 +39,9 @@ def UpdateSymbols():
 
 def GetMAHour(ticker=None):
     if ticker:
-        pass
+        ti = TechIndicators(key=os.environ['AlphaKey'], output_format='json')
+        data, meta = ti.get_sma(symbol=ticker, interval='1min', time_period=60, series_type='close')
+        return data
     return None
 
 def GetMADay(ticker=None):
@@ -97,26 +76,24 @@ if __name__ == '__main__':
     stocks = []
     run = True
     buy = True
-    stocksChecked = False
+    stocksWeeklyChecked = False
+    stocksHourlyChecked = False
     stocksRefreshed = False
     openTime = 9
     closeTime = 16
-    ts = TimeSeries(key=os.environ['AlphaKey'], output_format='pandas')
-    ti = TechIndicators(key=os.environ['AlphaKey'], output_format='pandas')
     while run:
         #check if market is open
-        if ((time.localtime()[3] >= openTime and time.localtime()[3] <= closeTime) and time.localtime()[6]<5 and stocksChecked):
+        if ((time.localtime()[3] >= openTime and time.localtime()[3] <= closeTime) and time.localtime()[6]<5 and stocksWeeklyChecked and stocksHourlyChecked):
             print('...Monitoring Stocks...')
             #pull stock intraday for each stock
+            ts = TimeSeries(key=os.environ['AlphaKey'], output_format='json')
             for stock in stocks:
                 dataI, metaI = ts.get_intraday(symbol=stock.ticker, interval='1min', outputsize='full')
-                dataIp = dataI['4. close'].iloc[period-1::]
-                dataT, metaT = ti.get_sma(symbol=stock.ticker, interval='1min', time_period=60, series_type='close')
-                df = pd.concat([dataIp, dataT], axis=1)
-                df.plot()
-            plot.show()
-        #Market closed, collect daily and weekly intra
-        if(stocksChecked == False):
+                stock.SetIntraDay(dataI)
+                #list(my_dict)[0] #Dic is now insertion order in 3.7+
+
+        #Find Stocks, Collect Weekly SMA, remove downers, collect hourly SMA
+        if(not stocksWeeklyChecked):
             print('...Collecting Weekly SMA...')
             #Check stocks for bad movers
             for stock in stocks:
@@ -141,14 +118,34 @@ if __name__ == '__main__':
                 print('Number Of Stock Options Left: ',len(stockList))
                 stocks = stocks + tempList
             else:
-                stocksChecked = True
+                stocksWeeklyChecked = True
+        #Get Hourly SMA trend for stocks        
+        elif(not stocksHourlyChecked):
+            print('...Collecting Hourly SMA...')
+            error = False
+            #Check stocks for bad movers
+            for stock in stocks:
+                print('...Collecting',stock.ticker,'...')
+                try:
+                    stock.SetMAHour(GetMAHour(stock.ticker))
+                except Exception as e:
+                    errror = True
+                    print('!!!Unable to collect hourly SMA for',stock.ticker,'\r\n',e)
+            if(not error):
+                stocksHourlyChecked = True
                     
 
         #Clear checks
-        if((time.localtime()[3] == 23)):
+        if((time.localtime()[3] == 23 and time.localtime()[4] == 59)):
             print('...Time to check stocks...')
-            stocksChecked = False
+            stocksWeeklyChecked = False
+            stocksWeeklyChecked = False
             stocksRefreshed = False
+        
+        #Check hourly SMA
+        if((time.localtime()[4] == 0)):
+            print('...Time to check hourly SMA...')
+            stocksHourlyChecked = False
         
         #sleep a minute
         print('...Wait 60s...')
--- stock/stock.py
+++ stock/stock.py
@@ -4,26 +4,31 @@ class Stock(object):
     def __init__(self, ticker=None, closingPrice=None):
         self.ticker=ticker
         self.closingPrice=closingPrice
+        self.intraDay={}
         self.smaWeek={}
         self.smaDay={}
-        self.smaMinute={}
+        self.smaHour={}
 
-    def SetMAMinute(self,intra=None):
-        self.smaMinute = intra
+    def SetMAHour(self,intra=None):
+        self.smaHour = intra
 
     def SetMADay(self,intra=None):
         self.smaDay = intra
 
     def SetMAWeek(self,intra=None):
         self.smaWeek = intra
+
+    def SetIntraDay(self,intra=None):
+        self.intraDay = intra
+        print(self.intraDay)
     
     def IsDownTrend(self):
         if bool(self.smaWeek):
             today = datetime.date.today()
             friday = today + datetime.timedelta((4-today.weekday())%7)
-            key1 = friday-datetime.timedelta(7*2)
+            key1 = friday-datetime.timedelta(7*1)
             key2 = friday-datetime.timedelta(7*5)
-            key3 = friday-datetime.timedelta(7*3)
+            key3 = friday-datetime.timedelta(7*2)
             key4 = friday-datetime.timedelta(7*6)
             if float(self.smaWeek[key1.strftime('%Y-%m-%d')]['SMA']) < float(self.smaWeek[key2.strftime('%Y-%m-%d')]['SMA']) and float(self.smaWeek[key3.strftime('%Y-%m-%d')]['SMA']) < float(self.smaWeek[key4.strftime('%Y-%m-%d')]['SMA']):
                 return True
